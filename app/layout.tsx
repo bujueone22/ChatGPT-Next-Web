ea82bef72937b7dbfd4e76b20a1a1da671f7d54b
bujueone22 <143797363+bujueone22@users.noreply.github.com>
2023-09-03 16:17:36 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "instant",
+  title: "instant AI写作",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
