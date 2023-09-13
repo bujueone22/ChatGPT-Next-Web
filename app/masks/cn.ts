2c5592256c9d08d1a3a0d540b39641900a521c47
bujueone22 <143797363+bujueone22@users.noreply.github.com>
2023-09-14 01:01:44 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -384,7 +384,7 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-3.5-turbo",
+      model: "gpt-4",
       temperature: 0.5,
       max_tokens: 2000,
       presence_penalty: 0,
